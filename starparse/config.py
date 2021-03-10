24a217845dfebde5932e46588a85b0737e3bab25
Alen Buhanec <alen.buhanec@gmail.com>
2021-03-10 18:03:13 +0000

    Clean up config, pull from env

--- starparse/config.py
+++ starparse/config.py
@@ -1,3 +1,16 @@
-UTF8 = False
-BYTE_STRUCT = False
-ORDERED_DICT = True
+"""Basic config."""
+
+import os
+
+__all__ = ('UTF8', 'BYTE_STRUCT', 'ORDERED_DICT')
+
+
+def _bool(env_var: str, default: bool) -> bool:
+    if env_var not in os.environ:
+        return default
+    return os.environ[env_var].upper() in ('1', 'T', 'TRUE')
+
+
+UTF8 = _bool('STARPARSE_UTF8', False)
+BYTE_STRUCT = _bool('STARPARSE_BYTE_STRUCT', False)
+ORDERED_DICT = _bool('STARPARSE_ORDERED_DICT', True)
